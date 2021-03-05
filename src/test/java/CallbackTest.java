6a88a4c98d46d9531b5443cb4b301e1113096852
Shurkovalina <shurkowalina@yandex.ru>
2021-03-05 13:53:38 +0300

    second task

--- src/test/java/CallbackTest.java
+++ src/test/java/CallbackTest.java
@@ -1,4 +1,3 @@
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
@@ -16,5 +15,15 @@ public class CallbackTest {
         $("[data-test-id=agreement]").click();
         $("button").click();
         $(".paragraph").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    void shouldSubmitIncorrectRequest() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Mor");
+        $("[data-test-id=phone] input").setValue("+79200000000");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 }
